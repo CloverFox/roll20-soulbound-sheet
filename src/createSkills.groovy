56529b25dd07cefc4a94370890d0b536d54784e1
CloverFox <cloverfox@protonmail.ch>
2020-06-10 19:01:17 +0100

    prettyfied attributes and skills

--- src/createSkills.groovy
+++ src/createSkills.groovy
@@ -1,13 +1,12 @@
 def template = """<div class="desc">
-            <span>Athletics</span>
+            <button type="roll" class="sheet-text-button"
+                    value="@{character_name} uses Athletics against DN ?{Target DN? |1|2|3|4|5|6}: [[(@{athletics_attribute}+@{athletics_1}+@{athletics_2}+@{athletics_3})d6>?{Target DN? |1|2|3|4|5|6}]] successes with [[@{athletics_focus_1} + @{athletics_focus_2} + @{athletics_focus_3}]] focus points"
+                    name="roll_athletics">Athletics</button>
             <select class="attribute-select" name="attr_athletics_attribute">
                 <option value="@{body}" selected="selected">body</option>
                 <option value="@{mind}">mind</option>
                 <option value="@{soul}">soul</option>
             </select>
-            <button type="roll"
-                    value="@{character_name} uses Athletics against DN ?{Target DN? |1|2|3|4|5|6}: [[(@{athletics_attribute}+@{athletics_1}+@{athletics_2}+@{athletics_3})d6>?{Target DN? |1|2|3|4|5|6}]] successes with [[@{athletics_focus_1} + @{athletics_focus_2} + @{athletics_focus_3}]] focus points"
-                    name="roll_athletics"></button>
         </div>
         <div class="training">
             <input type="checkbox" name="attr_athletics_1" value="1">
@@ -51,3 +50,9 @@ list.each {
     println(temp.replace("Athletics", "$it"))
 }
 
+//note, add this in the middle to create a new block
+//        </div>
+//        <div class="skills">
+//            <div></div>
+//            <div>Training</div>
+//            <div>Focus</div>
